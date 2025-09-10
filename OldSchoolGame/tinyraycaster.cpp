cdc3ae8bab246e5c11a4ec4f024ada4455a2499d
ber1ii <vujkovicgh@gmail.com>
2025-09-10 13:46:13 +0200

    Added first animation, camera panning in a 360 degree view, saving 120 images and then converting to gif

--- OldSchoolGame/tinyraycaster.cpp
+++ OldSchoolGame/tinyraycaster.cpp
@@ -9,6 +9,27 @@
 #include <string>
 #include <vector>
 
+// Define a 16x16 map
+constexpr size_t map_w = 16;
+constexpr size_t map_h = 16;
+constexpr size_t tileSize = 32;
+const char map[] = "0000222222220000"
+                   "1              0"
+                   "1      11111   0"
+                   "1     0        0"
+                   "0     0  1110000"
+                   "0     3        0"
+                   "0   10000      0"
+                   "0   0   11100  0"
+                   "0   0   0      0"
+                   "0   0   1  00000"
+                   "0       1      0"
+                   "2       1      0"
+                   "0       0      0"
+                   "0 0000000      0"
+                   "0              0"
+                   "0002222222200000";
+
 // Representing colors in a 32bit integer
 constexpr uint32_t packColor(const uint8_t r, const uint8_t g, const uint8_t b,
                              const uint8_t a = 255) noexcept {
@@ -99,147 +120,148 @@ int main() {
   constexpr size_t out_w = win_w * 2;
   constexpr size_t out_h = win_h;
 
-  std::vector<uint32_t> framebuffer(out_w * out_h);
-
-  // Minimap floor color
-  for (size_t j = 0; j < win_h; ++j) {
-    for (size_t i = 0; i < win_w; ++i) {
-      framebuffer[i + j * out_w] = packColor(245, 245, 220);
-    }
-  }
-
-  for (size_t j{}; j < win_h; j++) {
-    for (size_t i = win_w; i < out_w; i++) {
-      if (j < win_h / 2) {
-        framebuffer[i + j * out_w] = packColor(245, 245, 220); // White
-      } else {
-        framebuffer[i + j * out_w] = packColor(245, 245, 220); // Beige
-      }
-    }
-  }
-
-  // Define a 16x16 map
-  constexpr size_t map_w = 16;
-  constexpr size_t map_h = 16;
-  const char map[] = "0000222222220000"
-                     "1              0"
-                     "1      11111   0"
-                     "1     0        0"
-                     "0     0  1110000"
-                     "0     3        0"
-                     "0   10000      0"
-                     "0   0   11100  0"
-                     "0   0   0      0"
-                     "0   0   1  00000"
-                     "0       1      0"
-                     "2       1      0"
-                     "0       0      0"
-                     "0 0000000      0"
-                     "0              0"
-                     "0002222222200000";
-  constexpr size_t tileSize = 32;
-
-  // Map drawing
-  for (size_t j = 0; j < map_h; ++j) {
-    for (size_t i = 0; i < map_w; ++i) {
-      char tile = map[j * map_w + i];
-      if (tile != ' ') {
-        // Walls = cyan
-        uint32_t color = wallColor(tile);
-        drawRectangle(framebuffer, out_w, out_h, i * tileSize, j * tileSize,
-                      tileSize, tileSize, color);
-      }
-    }
-  }
+  constexpr int numFrames = 120; // 360deg angle
 
   // Drawing the player
   float player_x = 3.5f * tileSize;
   float player_y = 3.5f * tileSize;
 
-  constexpr int player_size = 5;
-  int half_size = player_size / 2;
+  for (int frame{}; frame < numFrames; frame++) {
+    std::vector<uint32_t> framebuffer(out_w * out_h, packColor(0, 0, 0));
 
-  drawRectangle(framebuffer, out_w, out_h,
-                static_cast<int>(player_x - half_size),
-                static_cast<int>(player_y - half_size), player_size,
-                player_size, packColor(0, 0, 0));
+    // Compute angle forr this frame
+    float player_a = 2 * M_PI * frame / numFrames;
 
-  // Player direction
-  float player_a = M_PI / 4.0f;
+    // Minimap floor color
+    /*for (size_t j = 0; j < win_h; ++j) {
+      for (size_t i = 0; i < win_w; ++i) {
+        framebuffer[i + j * out_w] = packColor(245, 245, 220);
+      }
+    }
 
-  // FOV rays
-  float fov = M_PI / 3.0f;
-  int numRays = win_w;
+    for (size_t j{}; j < win_h; j++) {
+      for (size_t i = win_w; i < out_w; i++) {
+        if (j < win_h / 2) {
+          framebuffer[i + j * out_w] = packColor(245, 245, 220); // White
+        } else {
+          framebuffer[i + j * out_w] = packColor(245, 245, 220); // Beige
+        }
+      }
+    } */
 
-  // FOV Direction minimap
-  for (int k = 0; k < numRays; k++) {
-    float t = float(k) / (numRays - 1);
-    float ang = player_a - fov / 2 + fov * t;
-    float dx = cos(ang), dy = sin(ang);
-    int x1 = int(player_x + dx * 40);
-    int y1 = int(player_y + dy * 40);
-    drawLine(framebuffer, out_w, out_h, int(player_x), int(player_y), x1, y1,
-             packColor(57, 255, 20));
-  }
+    // Background filling
+    for (size_t j{}; j < win_h; j++) {
+      for (size_t i{}; i < win_w; i++)
+        framebuffer[j * out_w + i] = packColor(245, 245, 220); // minimap bg
 
-  // 3D Rendering into right half
-  for (int k{}; k < numRays; k++) {
+      for (size_t i{win_w}; i < out_w; i++) {
+        if (j < win_h / 2)
+          framebuffer[j * out_w + i] = packColor(245, 245, 220); // ceiling
+        else
+          framebuffer[j * out_w + i] = packColor(245, 245, 220); // floor cream
+      }
+    }
 
-    float angle =
-        player_a - fov / 2 + fov * (static_cast<float>(k) / (numRays - 1));
-    // player_a - fov/2 is the left most ray, while adding fov * k/numRays
-    // creates player_a + fov/2
+    // Map drawing
+    for (size_t j = 0; j < map_h; ++j) {
+      for (size_t i = 0; i < map_w; ++i) {
+        char tile = map[j * map_w + i];
+        if (tile != ' ') {
+          // Walls = cyan
+          uint32_t color = wallColor(tile);
+          drawRectangle(framebuffer, out_w, out_h, i * tileSize, j * tileSize,
+                        tileSize, tileSize, color);
+        }
+      }
+    }
 
-    float dirX = std::cos(angle); // Directional vectors
-    float dirY = std::sin(angle);
+    constexpr int player_size = 5;
+    int half_size = player_size / 2;
 
-    // Raymcarching until wall
-    float rayX = player_x;
-    float rayY = player_y;
-    float dist = 0.f;
-    const float step = 1.f;
-    char hitTile = ' ';
+    drawRectangle(framebuffer, out_w, out_h,
+                  static_cast<int>(player_x - half_size),
+                  static_cast<int>(player_y - half_size), player_size,
+                  player_size, packColor(0, 0, 0));
 
-    while (true) {
-      int mapX = static_cast<int>(rayX / tileSize); // y index
-      int mapY = static_cast<int>(rayY / tileSize); // x index
+    // Player direction
+    // float player_a = M_PI / 4.0f;
 
-      if (mapX < 0 || mapY < 0 || mapX >= (int)map_w || mapY >= (int)map_h)
-        break;
+    // FOV rays
+    float fov = M_PI / 3.0f;
+    int numRays = win_w;
 
-      char tile = map[mapY * map_w + mapX]; // mapY * map_w = correct row (eg
-                                            // row 2*16) + mapX is the column
-      if (tile != ' ') {
-        hitTile = tile;
-        break;
+    // FOV Direction minimap
+    for (int k{}; k < numRays; k++) {
+      float t = float(k) / (numRays - 1);
+      float ang = player_a - fov / 2 + fov * t;
+      float dx = cos(ang), dy = sin(ang);
+      int x1 = int(player_x + dx * 40);
+      int y1 = int(player_y + dy * 40);
+      drawLine(framebuffer, out_w, out_h, int(player_x), int(player_y), x1, y1,
+               packColor(57, 255, 20));
+    }
+
+    // 3D Rendering into right half
+    for (int k{}; k < numRays; k++) {
+
+      float angle =
+          player_a - fov / 2 + fov * (static_cast<float>(k) / (numRays - 1));
+      // player_a - fov/2 is the left most ray, while adding fov * k/numRays
+      // creates player_a + fov/2
+
+      float dirX = std::cos(angle); // Directional vectors
+      float dirY = std::sin(angle);
+
+      // Raymcarching until wall
+      float rayX = player_x;
+      float rayY = player_y;
+      float dist = 0.f;
+      const float step = 1.f;
+      char hitTile = ' ';
+
+      while (true) {
+        int mapX = static_cast<int>(rayX / tileSize); // y index
+        int mapY = static_cast<int>(rayY / tileSize); // x index
+
+        if (mapX < 0 || mapY < 0 || mapX >= (int)map_w || mapY >= (int)map_h)
+          break;
+
+        char tile = map[mapY * map_w + mapX]; // mapY * map_w = correct row (eg
+                                              // row 2*16) + mapX is the column
+        if (tile != ' ') {
+          hitTile = tile;
+          break;
+        }
+
+        rayX += dirX * step; // Store each ray while moving forward
+        rayY += dirY * step; // and return it to mapX and mapY
+        dist += step;
       }
 
-      rayX += dirX * step; // Store each ray while moving forward
-      rayY += dirY * step; // and return it to mapX and mapY
-      dist += step;
+      int wall_h = (int)((win_h * tileSize) / (dist + 0.0001f));
+      // screenHeight = wall_h, realHeight = tileSize, focalLength = win_h,
+      // distance = measured ray length
+      // screenHeight = (realHeight * focalLength) / distance
+
+      int y0 = win_h / 2 - wall_h / 2; // locking y axis
+      int y1 = win_h / 2 + wall_h / 2;
+
+      if (y0 < 0)
+        y0 = 0;
+      if (y1 > (int)win_h)
+        y1 = win_h;
+
+      uint32_t color = wallColor(hitTile);
+
+      int col = win_w + k;
+      for (int y = y0; y < y1; y++) {
+        framebuffer[y * out_w + col] = color;
+      }
     }
 
-    int wall_h = (int)((win_h * tileSize) / (dist + 0.0001f));
-    // screenHeight = wall_h, realHeight = tileSize, focalLength = win_h,
-    // distance = measured ray length
-    // screenHeight = (realHeight * focalLength) / distance
-
-    int y0 = win_h / 2 - wall_h / 2; // locking y axis
-    int y1 = win_h / 2 + wall_h / 2;
-
-    if (y0 < 0)
-      y0 = 0;
-    if (y1 > (int)win_h)
-      y1 = win_h;
-
-    uint32_t color = wallColor(hitTile);
-
-    int col = win_w + k;
-    for (int y = y0; y < y1; y++) {
-      framebuffer[y * out_w + col] = color;
-    }
+    char fname[64];
+    sprintf(fname, "frame_%03d.ppm", frame);
+    dropPpmImage(fname, framebuffer, out_w, out_h);
   }
-
-  dropPpmImage("3dmodel.ppm", framebuffer, out_w, out_h);
   return 0;
 }
